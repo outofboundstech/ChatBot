080297f3197d320ccf541756b83a8184d1ca83bb
Marcel Oomens <mail@marceloomens.com>
2016-04-24 23:27:50 +0800

    Refector client js
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,11 +11,43 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+// import "phoenix_html"
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-import socket from "./socket"
+var client = document.getElementById('client')
+  , app = Elm.embed(Elm.Client, client, { connected: false, inbox: "", log: "" })
+
+var connected = app.ports.connected.send
+  , handle = app.ports.inbox.send
+  , log = app.ports.log.send
+
+import {Socket} from "phoenix"
+
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+
+socket.onOpen( () => console.log("socket open"))
+socket.onClose( () => console.log("socket close"))
+socket.onError( () => console.log("socket error"))
+
+socket.connect()
+
+let channel = socket.channel("chats:lobby", {})
+
+channel.onClose( () => { connected(false), log("Connection closed") })
+channel.onError( () => { connected(false), log("Connection dropped") })
+
+channel.join()
+  .receive("ok", resp => { connected(true), log("Connection established") })
+  .receive("error", resp => { log("Unable to connect") })
+
+function transport(payload) {
+  channel.push("ping", payload)
+    .receive("pong", ({payload}) => { handle(payload) })
+    .receive("error", e => { console.log(e) })
+}
+
+app.ports.transport.subscribe(transport)
